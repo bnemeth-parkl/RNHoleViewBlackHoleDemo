806f337c3d33dacd6d38b8d9ce05a407a4c38c9f
Bence Németh <66995534+bnemeth-parkl@users.noreply.github.com>
2024-10-04 12:14:23 +0200

    hole view demo

---
+++ App.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {ERNHoleViewTimingFunction, RNHoleView} from 'react-native-hole-view';
+
+const animationSettings = {
+  timingFunction: ERNHoleViewTimingFunction.EASE_IN_OUT,
+  duration: 200,
+};
+
+function Section({children, title}) {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'light';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [animation, setAnimation] = useState({...animationSettings});
+
+  return (
+    <>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            <Section title="Step One">
+              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+              screen and then come back to see your edits.
+            </Section>
+            <Section title="See Your Changes">
+              <ReloadInstructions />
+            </Section>
+            <Section title="Debug">
+              <DebugInstructions />
+            </Section>
+            <Section title="Learn More">
+              Read the docs to discover what to do next:
+            </Section>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <RNHoleView
+        style={styles.holeBackground}
+        holes={[{x: 100, y: 400, height: 50, width: 200}]}
+        animation={animation}
+        onAnimationFinished={() => setAnimation(undefined)}
+      />
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  holeBackground: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#00000088',
+  },
+});
